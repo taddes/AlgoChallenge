ebbc2c11580ea3689cff40ecf0bf89690ec324cb
Taddes Korris <taddes.korris@gmail.com>
2020-04-11 20:43:49 -0400

    added test and solution for js

--- max_char/js/index.js
+++ max_char/js/index.js
@@ -8,4 +8,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+
 // Building a character map
+const string = 'Hello There Trello Yello';
+const chars = {};
+
+for (let char of string) {
+  if (!chars[char]) {
+      chars[char] = 1
+  } else {
+      chars[char]++;
+  }
+}
+// Alternate version. If null reutrns, evals as falsy, puts in 1
+for (let char of string) {
+chars[char] = chars[char] + 1 || 1;
+  }
+
+// console.log(chars)
+
+function maxChar(string) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of string) {
+    if (charMap[char]) {
+        charMap[char]++;
+    } else {
+        charMap[char] = 1;
+    }
+  }
+
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+console.log(maxChar('Hello Velt'))
+
+module.exports = maxChar;
