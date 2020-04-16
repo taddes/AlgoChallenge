8c836d8897c8e00c99e6c43634d2c8a37893af7b
Taddes Korris <taddes.korris@gmail.com>
2020-04-15 22:22:53 -0400

    completed capitalize options py and js

--- sentence_capitalization/js/index.js
+++ sentence_capitalization/js/index.js
@@ -7,14 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-    let word_list = str.split(' ');
-    let title_list = [];
+// function capitalize(str) {
+//     let word_list = str.split(' ');
+//     let title_list = [];
 
-    for (let word of word_list) {
-        title_list.push(word.slice(0,1).toUpperCase() + word.slice(1))
+//     for (let word of word_list) {
+//         title_list.push(word.slice(0,1).toUpperCase() + word.slice(1))
+//     }
+//     return title_list.join(' ')
+// }
+
+// Alternate Solution
+` 
+Create enpty string, result
+For each char in string
+    IF char to the left a space
+        capitalize and add to result
+    ELSE
+        Add to result
+`
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            resuilt += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
     }
-    return title_list.join(' ')
+    return result
 }
 
 console.log(capitalize('Tis is the greatest function'))
--- sentence_capitalization/py/capitalize.py
+++ sentence_capitalization/py/capitalize.py
@@ -14,11 +14,25 @@
 #     return string.title()
 
 # Solution 2 - Less Graceful loop solution
-def capital(string):
-    sentence_list = string.split(' ')
-    capitalized_list = []
-    for word in sentence_list:
-        capitalized_list.append(word[0].upper() + word[1:])
-    return ' '.join(capitalized_list)
+# def capital(string):
+#     sentence_list = string.split(' ')
+#     capitalized_list = []
+#     for word in sentence_list:
+#         capitalized_list.append(word[0].upper() + word[1:])
+#     return ' '.join(capitalized_list)
 
+
+# Solution 3
+def capital(string):
+    capital_string = ''
+    for index, letter in enumerate(string[1:]):
+        if index == 0:
+            capital_string += string[index]
+        elif string[index - 1] == ' ':
+            capital_string += string[index].upper()
+        else:
+            capital_string += string[index]
+    return capital_string
+
+        
 print(capital('This is the state!'))
