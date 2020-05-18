6c994ff4de00f735291c759cfae7e4fff171561f
Taddes Korris <taddes.korris@gmail.com>
2020-05-18 17:16:10 -0400

    linked list methods

--- linkedList/py/linked_list.py
+++ linkedList/py/linked_list.py
@@ -2,3 +2,18 @@ class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_first(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    
+    def __repr__(self):
+        return str(self.head)
+
+l = LinkedList()
+l.insert_first(123)
+print(l)
