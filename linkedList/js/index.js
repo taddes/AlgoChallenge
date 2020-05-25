ab67ae07a883b0d8ac936485b0453772e221f848
Taddes Korris <taddes.korris@gmail.com>
2020-05-25 13:38:53 -0400

    get at node method

--- linkedList/js/index.js
+++ linkedList/js/index.js
@@ -127,7 +127,21 @@ class LinkedList {
     previous.next = previous.next.next
   }
 
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
 
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
 
 
 }
